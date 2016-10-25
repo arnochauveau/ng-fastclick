9abe3a5f1932473e2aeb3862b2a79b6d9883b353
arno chauveau <arno.chaveau@delen.be>
2016-10-25 13:10:35 +0200

    support npm + webpack

--- dist/index.js
+++ dist/index.js
@@ -840,6 +840,12 @@
 	}
 }());
 
+
+/* commonjs package manager support (eg componentjs) */
+if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports){
+    module.exports = 'ng-fastclick';
+}
+
 angular.module('ng-fastclick', []).run(function() {
   FastClick.attach(document.body)
 });
